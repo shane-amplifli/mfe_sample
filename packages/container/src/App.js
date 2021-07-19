a9dbcd871af3748f4dcf2faadb56ae64cf6240e9
shane-amplifli <shane.singh@amplifli.com.au>
2021-07-19 16:01:49 +1000

    added header component to the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
-    return (
-			<div>
-				<h1>container app</h1>
-				<MarketingApp />;
-			</div>
-		); 
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />;
+      </div>
+    </BrowserRouter>
+  );
 };
