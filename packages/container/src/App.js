746d61d17eaddfa77d3640ba13abb2cae717acf8
shane-amplifli <shane.singh@amplifli.com.au>
2021-07-20 12:06:51 +1000

    initial state for memory history, lazy loading, loading bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
-import MarketingApp from "./components/MarketingApp";
+
 import Header from "./components/Header";
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -17,7 +21,12 @@ export default () => {
       <BrowserRouter>
         <div>
           <Header />
-          <MarketingApp />;
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
